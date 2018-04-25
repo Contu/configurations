0ab984839455a20d4bf720e23cd34fc9a54c299f
Contu <Salvatore.Contu@gmail.com>
2018-04-25 11:53:02 +0200

    added i3blocks.conf

--- bin/syncConf.sh
+++ bin/syncConf.sh
@@ -5,6 +5,7 @@ CODE=~/code/configurations
 rsync -av ~/.config/i3 $CODE
 rsync -av ~/.emacs $CODE/.emacs
 rsync -av ~/.vimrc $CODE/.vimrc
+rsync -av ~/.i3blocks.conf $CODE/.i3blocks.conf
 rsync -av ~/.Xdefaults $CODE/.Xdefaults
 rsync -av ~/bin $CODE
 rsync -av ~/.bashConf.sh $CODE/.bashConf.sh

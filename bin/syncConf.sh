ba3309976e89bf5ae64d83c4543ad1dced147c8b
Contu <Salvatore.Contu@gmail.com>
2018-08-28 22:43:09 +0200

    newsboat configuration

--- bin/syncConf.sh
+++ bin/syncConf.sh
@@ -6,6 +6,8 @@ rsync -av ~/.config/feh $CODE
 rsync -av ~/.urxvt $CODE
 rsync -av ~/.ncmpcpp $CODE
 rsync -av ~/.config/i3 $CODE
+rsync -av ~/.newsboat/config $CODE/newsboat
+rsync -av ~/.newsboat/urls $CODE/newsboat
 rsync -av ~/.emacs $CODE/.emacs
 rsync -av ~/.vimrc $CODE/.vimrc
 rsync -av ~/.mailcap $CODE/.mailcap

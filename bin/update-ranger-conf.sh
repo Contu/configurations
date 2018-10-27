79cafcf95dbdfe36b0904d5d1fa50859a93f434e
Contu <Salvatore.Contu@gmail.com>
2018-10-27 14:27:59 +0200

    update ranger configuration

--- bin/update-ranger-conf.sh
+++ bin/update-ranger-conf.sh
@@ -5,10 +5,10 @@ cp -f ~/.config/ranger/rc.conf ~/.config/ranger/rc.conf.bak
 LINE_START_CUSTOM_MAPPING=$(grep -n "Custom mapping" ~/.config/ranger/rc.conf | cut -d':' -f1)
 
 if [[ $LINE_START_CUSTOM_MAPPING = '' ]]; then
-    LINE_START_CUSTOM_MAPPING=$(wc -l ~/.config/ranger/rc.conf | cut -d' ' -f1)
+    cp ~/.config/ranger/rc.conf ~/.config/ranger/rc.conf.temp
+else
+    head -n $(expr $LINE_START_CUSTOM_MAPPING - 1) ~/.config/ranger/rc.conf > ~/.config/ranger/rc.conf.temp
 fi
-
-head -n $(expr $LINE_START_CUSTOM_MAPPING - 1) ~/.config/ranger/rc.conf > ~/.config/ranger/rc.conf.temp
 
 cat ~/.ranger-conf.txt >> ~/.config/ranger/rc.conf.temp
 

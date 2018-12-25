16ff601e0e9d7b73a0bf48dee6517102082dac1f
Contu <Salvatore.Contu@gmail.com>
2018-12-25 16:54:00 +0100

    improved mount device script

--- bin/mountIntercativeRemovableDrive.sh
+++ bin/mountIntercativeRemovableDrive.sh
@@ -14,23 +14,25 @@ function create-mount-point {
     echo "$LABEL"
 }
 
-DEVICE=$(lsblk -ln -o NAME | awk '!/sda/ {print $0}' | dmenu -i -p MOUNT)
+DEVICE=$(lsblk -lo name,rm,label|awk '{if ($2 == 1) { print $1"["$3"]"}}' | dmenu -i -p MOUNT)
 
-FSTYPE=`lsblk -no FSTYPE /dev/$DEVICE`
+DEVICE=$(echo $DEVICE | cut -d'[' -f1)
+FSTYPE=$(lsblk -lno fstype /dev/$DEVICE)
+LABEL=$(lsblk -lno label /dev/$DEVICE)
 
 if [[ $FSTYPE == 'vfat' ]]; then
-    LABEL=`sudo dosfslabel $1`
+    #LABEL=`sudo dosfslabel $1`
     LABEL=`create-mount-point $LABEL`
     sudo mount -o uid=`id -u`,gid=`id -g` /dev/$DEVICE ~/$LABEL
-    notify-send "Mounted successfully"
+    notify-send "$LABEL Mounted successfully"
 elif [[ $FSTYPE == 'ext4' ]]; then
-    LABEL=`sudo e2label /dev/$DEVICE`
+    #LABEL=`sudo e2label /dev/$DEVICE`
     create-mount-point $LABEL
     sudo mount /dev/$DEVICE ~/$LABEL
     sudo chown -R `id -u`:`id -g` ~/$LABEL
-    notify-send "Mounted successfully"
+    notify-send "$LABEL Mounted successfully"
 elif [[ $FSTYPE == 'iso9660' ]]; then
-    LABEL=`lsblk -ln -o NAME,LABEL | awk -v DEVICE="$DEVICE" '($1==DEVICE) {print $2}'`
+    #LABEL=`lsblk -ln -o NAME,LABEL | awk -v DEVICE="$DEVICE" '($1==DEVICE) {print $2}'`
     create-mount-point $LABEL
     sudo mount --read-only /dev/$DEVICE ~/$LABEL
     notify-send "Mounted successfully"

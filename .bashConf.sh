c8b7ec1dd870b419f1755adf620c4df61605c1d3
Contu <Salvatore.Contu@gmail.com>
2019-06-29 12:44:15 +0200

    cleaning

--- .bashConf.sh
+++ .bashConf.sh
@@ -58,3 +58,6 @@ gr() {
 	echo "Wrong arguments !!!"
     fi
 }
+
+[[ -f /usr/share/fzf/completion.bash ]] && . /usr/share/fzf/completion.bash
+[[ -f /usr/share/fzf/key-bindings.bash ]] && . /usr/share/fzf/key-bindings.bash
